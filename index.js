645d1c127e5e12c58a6d0f274bc38fe39ded70f7
fillika <fillika@users.noreply.github.com>
2019-06-21 21:40:27 +0300

    finish
    Закончил игру. Добавил квадраты разного цвета, связал инпуты

--- index.js
+++ index.js
@@ -1,17 +1,32 @@
 var $start = document.querySelector('#start');
 var $game = document.querySelector('#game');
-var $time = document.querySelector('#time'); 
+var $time = document.querySelector('#time');
+var $result = document.querySelector('#result');
+var $timeHeader = document.querySelector('#time-header');
+var $resultHeader = document.querySelector('#result-header');
+var $gameTime = document.querySelector('#game-time');
 
 var score = 0; // Переменная, для подсчета результата
 var isGameStarted = false; // Переменная для отслеживания состояния игры (изначально false, после начала игры становится true, когда время кончается, меняем на false)
 
 $start.addEventListener('click', startGame); // Начало игры по клику
-
 $game.addEventListener('click', handleBoxClick); // функция, которая при клике будет заново создавать квадрат
+$gameTime.addEventListener('input', setGameTime);
+
+function show($el) {
+  $el.classList.remove('hide');
+}
+
+function hide($el) {
+  $el.classList.add('hide');
+}
 
 function startGame() { // Функция начала игры
+  score = 0;
+  setGameTime ();
+  $gameTime.setAttribute('disabled', true);
   isGameStarted = true;
-  $start.classList.add('hide');  // Добавляем класс hide и скрываем кнопку
+  hide($start);  // Добавляем класс hide и скрываем кнопку
   $game.style.backgroundColor = '#fff'; // Добавляем белый фон
 
   var interval = setInterval(function() {  // Функция setInterval производит действие каждые x милисекунд
@@ -27,8 +42,27 @@ function startGame() { // Функция начала игры
   renderBox();
 }
 
+function setGameScore () { // Функция выводит результат игры в виде строки
+  $result.textContent = score.toString();
+}
+
+function setGameTime () { // Функция задает время игры
+  var time = +$gameTime.value;
+  $time.textContent = time.toFixed(1);
+  show($timeHeader);
+  hide($resultHeader);
+}
+
 function endGame () {  // Функция окончания игры
   isGameStarted = false;
+  setGameScore ();
+  $game.innerHTML = '';
+  show($start);
+  $game.style.backgroundColor = '#ccc';
+  hide($timeHeader);
+  show($resultHeader);
+  $gameTime.removeAttribute('disabled');
+ 
 }
 
 function handleBoxClick(event) { // Если при клике по div с data-атрибутом box у нас значение true, то мы запускаем renderBox заново и прибавляем к результату +1
@@ -52,7 +86,7 @@ function renderBox() {
 
   box.style.height = box.style.width = boxSize + 'px'; // Задали высоту и ширину
   box.style.position = 'absolute'; // Задали позиционирование
-  box.style.backgroundColor = '#000'; // Задали цвет
+  box.style.backgroundColor = 'rgb(' + getRandom(0, 255) + ',' + getRandom(0, 255) + ',' + getRandom(0, 255) + ')'; // Задали цвет
   box.style.top = getRandom(0, maxTop) + 'px'; // Задали позицию
   box.style.left = getRandom(0, maxLeft) + 'px'; // Задали позицию
   box.style.cursor = 'pointer'; // Курсор поинтер
